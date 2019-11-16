8b52cc12d78af6ea59e315c34edb25c889b63992
Karolina Gustawska <karolina.gustawska@gmail.com>
2019-11-16 09:57:48 +0100

    Find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -36,8 +36,11 @@
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [done] find the correct article using the selector (value of 'href' attribute) */
   
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
   }
   
